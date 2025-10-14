527c72df716868fbf4cd985301ec66b7613ba38e
Kosiska <164752888+Kosiska@users.noreply.github.com>
2025-10-15 02:34:51 +0300

    Update pract1_7.cpp

--- pract1_7.cpp
+++ pract1_7.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Windows.h>
+#include <format>
 using namespace std;
 
 
@@ -14,17 +15,17 @@ int main(){
     setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
     double sum = 0;
-    for (i=0; <5; ){
+    for (int i=0; i < 5;i++){
         double h,s;
-        cout << format("Введите кол-во отработанных часов и почасовую ставку {} работника ", i+1)
-        cin >> h, s;
+        cout << "Введите кол-во отработанных часов и почасовую ставку работника номер " << i+1;
+        cin >> h >> s;
         sum += f(h, s);
-        cout >> format("Рабочий номер {} заработал {}", i+1, f(h,s));
+        cout << "Рабочий номер " << i+1 << " заработал " << f(h,s);
     }
     
     cout << "зредняя зп в бригаде: " << sum/5;
 
     return 0;
 }
-
