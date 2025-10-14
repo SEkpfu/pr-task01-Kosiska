e2653352caee4aa8e9a84836c5d4b0e7045fd004
Kosiska <164752888+Kosiska@users.noreply.github.com>
2025-10-15 02:33:19 +0300

    Update pract1_4.cpp

--- pract1_4.cpp
+++ pract1_4.cpp
@@ -2,32 +2,21 @@
 using namespace std;
 
 
-void f(double& h,double& s,double& p, double& salary, double& nalog, double& total){
+void f(double& h, double& s, double& p, double& salary, double& nalog, double& total){
+    cout << "Введите часы работы, ставку и премию работника: ";
+    cin >> h >> s >> p;
     salary = h * s * (1 + p / 100);
     nalog = salary * 0.13;
     total = salary - nalog;
-
 }
 
 
 int main(){
     setlocale(0, "");
-    double h, s, p;
-    double salary, nalog, total;
-
-    cout << "Ââåäèòå êîë-âî îòðàáîòàííûõ ÷àñîâ: ";
-    cin >> h ;
-    cout << "Ââåäèòå ñòàâêó ïî÷àñîâîé îïëàòû: ";
-    cin >> s;
-    cout << "Ââåäèòå % ïðåìèè ";
-    cin >> p;
-    
-    f(h,s,p,salary, nalog, total);
-    
-    cout << "Îáùàÿ çàðàáîòàííàÿ ñóììà ñîñòàâëÿåò " << salary << endl;
-    cout << "Ïîäîõîäíûé íàëîã ñîñòàâëÿåò " << nalog << endl;
-    cout << "Ñóììà, ïîëó÷àåìàÿ íà ðóêè, ñîñòàâëÿåò " << total << endl;
+    double h, s, p, nalog, salary, total;
+    f(h, s, p, salary, nalog, total);
+    cout << "Общая сумма, сумма налога и зп на руки: ";
+    cout << salary << " " << nalog << " " << total;
 
     return 0;
 }
-
